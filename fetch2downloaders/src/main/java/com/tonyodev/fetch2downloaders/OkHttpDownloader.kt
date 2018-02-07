6f70c411a91d5db43da27d9ceaf0e7841ff67906
Tonyo Francis <tonyo.f@irokopartners.com>
2018-02-06 23:57:25 -0500

    refactored OkHttpDownloader

--- fetch2downloaders/src/main/java/com/tonyodev/fetch2downloaders/OkHttpDownloader.kt
+++ fetch2downloaders/src/main/java/com/tonyodev/fetch2downloaders/OkHttpDownloader.kt
@@ -16,7 +16,8 @@ import java.util.concurrent.TimeUnit
 open class OkHttpDownloader @JvmOverloads constructor(okHttpClient: OkHttpClient? = null)
     : Downloader {
 
-    val connections = Collections.synchronizedMap(HashMap<Downloader.Response, Response>())
+    //TODO: CHECK on this lint issue
+    protected val connections = Collections.synchronizedMap(HashMap<Downloader.Response, Response>())
 
     @Volatile
     var client: OkHttpClient = okHttpClient ?: OkHttpClient.Builder()
