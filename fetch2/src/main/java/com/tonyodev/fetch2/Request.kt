94aafbe5094406922537eb6f1eb9d996c508d07a
Tonyo Francis <tonyo.f@irokopartners.com>
2018-07-09 14:57:00 -0400

    request now supports parcelable

--- fetch2/src/main/java/com/tonyodev/fetch2/Request.kt
+++ fetch2/src/main/java/com/tonyodev/fetch2/Request.kt
@@ -1,5 +1,7 @@
 package com.tonyodev.fetch2
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.tonyodev.fetch2core.getUniqueId
 
 /**
@@ -12,7 +14,7 @@ open class Request constructor(
 
         /** The file eg(/files/download.txt) where the file will be
          * downloaded to and saved on disk.*/
-        val file: String) : RequestInfo() {
+        val file: String) : RequestInfo(), Parcelable {
 
     /** Unique Identifier. Used to identify a download.*/
     val id: Int = getUniqueId(url, file)
@@ -39,6 +41,57 @@ open class Request constructor(
     override fun toString(): String {
         return "Request(url='$url', file='$file', id=$id, groupId=$groupId, " +
                 "headers=$headers, priority=$priority, networkType=$networkType, tag=$tag)"
+    }
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest?.writeString(url)
+        dest?.writeString(file)
+        dest?.writeLong(identifier)
+        dest?.writeInt(groupId)
+        dest?.writeSerializable(HashMap(headers))
+        dest?.writeInt(priority.value)
+        dest?.writeInt(networkType.value)
+        dest?.writeString(tag)
+        dest?.writeInt(enqueueAction.value)
+        dest?.writeInt(if (downloadOnEnqueue) 1 else 0)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Request> {
+
+        @Suppress("UNCHECKED_CAST")
+        override fun createFromParcel(input: Parcel): Request {
+            val url = input.readString()
+            val file = input.readString()
+            val identifier = input.readLong()
+            val groupId = input.readInt()
+            val headers = input.readSerializable() as Map<String, String>
+            val priority = Priority.valueOf(input.readInt())
+            val networkType = NetworkType.valueOf(input.readInt())
+            val tag = input.readString()
+            val enqueueAction = EnqueueAction.valueOf(input.readInt())
+            val downloadOnEnqueue = input.readInt() == 1
+
+            val request = Request(url, file)
+            request.identifier = identifier
+            request.groupId = groupId
+            headers.forEach {
+                request.addHeader(it.key, it.value)
+            }
+            request.priority = priority
+            request.networkType = networkType
+            request.tag = tag
+            request.enqueueAction = enqueueAction
+            request.downloadOnEnqueue = downloadOnEnqueue
+            return request
+        }
+
+        override fun newArray(size: Int): Array<Request?> {
+            return arrayOfNulls(size)
+        }
     }
 
 }
