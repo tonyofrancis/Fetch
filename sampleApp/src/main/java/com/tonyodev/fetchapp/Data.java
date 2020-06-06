58e9f7086d30669886ff5a1ba0e4f71c14d2210b
Tonyo Francis <tonyo.f@irokopartners.com>
2020-06-06 11:02:30 -0400

    link updates

--- sampleApp/src/main/java/com/tonyodev/fetchapp/Data.java
+++ sampleApp/src/main/java/com/tonyodev/fetchapp/Data.java
@@ -15,8 +15,7 @@ public final class Data {
 
     public static final String[] sampleUrls = new String[]{
             "http://speedtest.ftp.otenet.gr/files/test100Mb.db",
-            "https://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_720p_stereo.ogg",
-            "http://media.mongodb.org/zips.json",
+            "https://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_720p_stereo.avi",
             "http://www.exampletonyotest/some/unknown/123/Errorlink.txt",
             "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Android_logo_2019.svg/687px-Android_logo_2019.svg.png",
             "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"};
